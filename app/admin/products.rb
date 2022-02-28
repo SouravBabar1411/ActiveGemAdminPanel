269cadbbbd341e847cdb63dd9b4617a30faa8400
sourav1411 <babarsourav12@gmail.com>
2022-02-28 17:08:10 +0530

    added assosiation organization and product moduls

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,17 +1,17 @@
-ActiveAdmin.register Organization do
-  permit_params :name
+ActiveAdmin.register Product do
+  permit_params :name, :description, :organization_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name
+  # permit_params :name, :description, :organization_id
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name]
+  #   permitted = [:name, :description, :organization_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
